e1050cd3f1852ac01088629e5014452cb0052bd6
Cyber Panel <bbosa.boscojohn@gmail.com>
2026-04-03 14:17:21 +0200

    connect git with cyberp

---
+++ config/system-addons.php
@@ -0,0 +1,66 @@
+<?php return array (
+  'admin_panel' => 
+  array (
+    'active' => '1',
+    'name' => NULL,
+    'identifier' => NULL,
+    'username' => 'steinsjb',
+    'purchase_key' => 'd17ab145-b16f-4315-a408-9bcd3405cf36',
+    'software_id' => 'MzE0NDg1OTc=',
+    'domain' => 'hubug.cloud',
+    'software_type' => 'product',
+    'errors' => 
+    array (
+    ),
+  ),
+  'vendor_panel' => 
+  array (
+    'active' => '0',
+    'username' => '',
+    'purchase_key' => '',
+    'software_id' => '',
+    'domain' => '',
+    'software_type' => 'addon',
+  ),
+  'user_app' => 
+  array (
+    'active' => '0',
+    'username' => '',
+    'purchase_key' => '',
+    'software_id' => '',
+    'domain' => '',
+    'software_type' => 'addon',
+  ),
+  'vendor_app' => 
+  array (
+    'active' => '0',
+    'username' => '',
+    'purchase_key' => '',
+    'software_id' => '',
+    'domain' => '',
+    'software_type' => 'addon',
+  ),
+  'deliveryman_app' => 
+  array (
+    'active' => '1',
+    'name' => '',
+    'identifier' => '',
+    'username' => '',
+    'purchase_key' => '',
+    'software_id' => 'MzYwODUwNDE=',
+    'domain' => '',
+    'software_type' => 'addon',
+    'errors' => 
+    array (
+    ),
+  ),
+  'theme_lifestyle' => 
+  array (
+    'active' => '0',
+    'username' => '',
+    'purchase_key' => '',
+    'software_id' => '',
+    'domain' => '',
+    'software_type' => 'addon',
+  ),
+);
